19f6e069594ae57ab49b0fce9276190cd6f5f600
MidasXIV <amanshaikh.arc@gmail.com>
2020-03-21 15:54:59 +0400

    Basic boiler plate for app - basic ESLint config - basic nodemon config - basic typescript config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Malang Dividend portfolio tracker / stimulator');
+});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
