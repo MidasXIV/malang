01906ada90359f594e9e686f5a80c20bcb4c9ebf
MidasXIV <amanshaikh.arc@gmail.com>
2020-04-15 01:32:02 +0400

    [ADD] template pages for portfolio,registration,overview
    - Sidebar enhancement make sidebar a flex container with justify content items.

--- src/routes/viewsRouter.ts
+++ src/routes/viewsRouter.ts
@@ -6,4 +6,16 @@ router.get('/', function (request: Request, response: Response) {
     response.render('index', { layout: 'main' });
 });
 
+router.get('/register', function (request: Request, response: Response) {
+    response.render('login', { layout: 'main' });
+});
+
+router.get('/portfolio', function (request: Request, response: Response) {
+    response.render('portfolio', { layout: 'main' });
+});
+
+router.get('/overview', function (request: Request, response: Response) {
+    response.render('overview', { layout: 'main' });
+});
+
 export default router;
