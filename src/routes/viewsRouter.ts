618f89eae6a959efc31f4dac77b791862015fd05
MidasXIV <amanshaikh.arc@gmail.com>
2020-05-02 00:08:06 +0400

    [ADD] Page not found view and route

--- src/routes/viewsRouter.ts
+++ src/routes/viewsRouter.ts
@@ -2,20 +2,29 @@ import Router from 'express';
 import { Request, Response } from 'express';
 const router = Router();
 
+/** dividend simulator page */
 router.get('/', function (request: Request, response: Response) {
     response.render('index', { layout: 'main' });
 });
 
+/** registration page */
 router.get('/register', function (request: Request, response: Response) {
     response.render('login', { layout: 'main' });
 });
 
+/** dividend portfolio page */
 router.get('/portfolio', function (request: Request, response: Response) {
     response.render('portfolio', { layout: 'main' });
 });
 
+/** overview page */
 router.get('/overview', function (request: Request, response: Response) {
     response.render('overview', { layout: 'main' });
+});
+
+/** Invalid page routes */
+router.get('/*', function (request: Request, response: Response) {
+    response.render('page-not-found', { layout: 'main' });
 });
 
 export default router;
