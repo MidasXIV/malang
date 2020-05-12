d0097b73a1cff66cf0dea253fd9cbd083f0fc502
MidasXIV <amanshaikh.arc@gmail.com>
2020-05-12 22:32:43 +0400

    Minor changes - updating package.json and package-lock.json - rename email-validator variable - remove double stroke left border to input group in main view.

--- src/util/email-validator.ts
+++ src/util/email-validator.ts
@@ -7,13 +7,14 @@
 
 export function validate(email: string): boolean {
 
-  const tester = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-
+  // eslint-disable-next-line no-useless-escape
+  const emailVerificationRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  
   if (!email) return false;
 
   if (email.length > 256) return false;
 
-  if (!tester.test(email)) return false;
+  if (!emailVerificationRegex.test(email)) return false;
 
   // Further checking of some things regex can't handle
   const [account, address] = email.split('@');
