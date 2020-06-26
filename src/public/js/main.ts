fcbcbf86cccfe843ca532e3b715c552096c4b4b7
MidasXIV <amanshaikh.arc@gmail.com>
2020-06-26 16:14:09 +0400

    Add webpack support to build client side TS and SCSS - added client side tsconfig & tsconfig for webpack - added webpack config with babel and node-sass support -

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,22 +1,47 @@
-import ApexCharts from 'apexcharts'
-((): void => {
-  console.log("removing global pull");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ApexCharts = require('apexcharts/dist/apexcharts.min.js');
+import '../css/main.scss';
 
-  const options = {
+export default class ApexChartComponent {
+
+  options = {
     chart: {
-      type: 'bar'
+      height: 280,
+      type: "area"
+    },
+    dataLabels: {
+      enabled: false
     },
     series: [
       {
-        name: 'sales',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+        name: "Series 1",
+        data: [45, 52, 38, 45, 19, 23, 2]
       }
     ],
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 0.7,
+        opacityTo: 0.9,
+        stops: [0, 90, 100]
+      }
+    },
     xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      categories: [
+        "01 Jan",
+        "02 Jan",
+        "03 Jan",
+        "04 Jan",
+        "05 Jan",
+        "06 Jan",
+        "07 Jan"
+      ]
     }
-  }
+  };
 
-  const chart = new ApexCharts(document.querySelector('#chart'), options)
-  chart.render()
-})();
+  public renderChart(): void {
+    const chart = new ApexCharts(document.querySelector("#chart"), this.options);
+    chart.render();
+  }
+}
