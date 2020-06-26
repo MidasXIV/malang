52b0126afe59265d453f8c348aa6c54bcc4a434d
MidasXIV <amanshaikh.arc@gmail.com>
2020-06-27 01:02:34 +0400

    Styling chart component

--- src/public/js/ApexChartComponent.ts
+++ src/public/js/ApexChartComponent.ts
@@ -8,25 +8,56 @@ export default class ApexChartComponent {
       height: 280,
       type: "area"
     },
+    grid: {
+      show: false,
+      // borderColor: '#90A4AE',
+      strokeDashArray: 0,
+      position: 'back',
+      xaxis: {
+        lines: {
+          show: false
+        }
+      },
+      yaxis: {
+        lines: {
+          show: false
+        }
+      },
+    },
     dataLabels: {
       enabled: false
     },
     series: [
       {
         name: "Series 1",
-        data: [45, 52, 38, 45, 19, 23, 2]
+        data: [45, 52, 38, 45, 19, 23, 2, 45, 75, 60]
       }
     ],
     fill: {
+      colors: ["#4d5c94", "#000000"],
       type: "gradient",
       gradient: {
-        shadeIntensity: 1,
+        shade: "dark",
+        shadeIntensity: 0.5,
         opacityFrom: 0.7,
-        opacityTo: 0.9,
+        opacityTo: 0.0,
         stops: [0, 90, 100]
       }
     },
+    stroke: {
+      width: 3,
+      colors: ["#4d5c94"]
+    },
     xaxis: {
+      labels: {
+        show: true,
+        style: {
+          colors: "#455059"
+        }
+      },
+      axisBorder: {
+        show: false
+      },
       categories: [
         "01 Jan",
         "02 Jan",
@@ -34,8 +65,19 @@ export default class ApexChartComponent {
         "04 Jan",
         "05 Jan",
         "06 Jan",
-        "07 Jan"
+        "07 Jan",
+        "08 Jan",
+        "09 Jan",
+        "10 Jan",
       ]
+    },
+    yaxis: {
+      labels: {
+        show: true,
+        style: {
+          colors: "#455059"
+        }
+      }
     }
   };
 
