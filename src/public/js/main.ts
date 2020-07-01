1b65c2a0e322adc283f81c3f30e6e9f6dfde60fa
MidasXIV <amanshaikh.arc@gmail.com>
2020-07-01 23:02:39 +0400

    testing chart components

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -2,12 +2,19 @@
  *     IMPORT all dependencies here
  *******************************************/
 
- import '../css/main.scss';
- 
- // Import chart component
- import ApexChartComponent from './ApexChartComponent';
- 
- const apexLineChart = new ApexChartComponent();
- apexLineChart.renderChart();
- 
- 
+import '../css/main.scss';
+
+// Import chart component
+import ApexChartComponent from './ApexChartComponent';
+
+const chartNode1: HTMLDivElement = document.querySelector("#chart1");
+const apexLineChartBasic = new ApexChartComponent(chartNode1);
+apexLineChartBasic.renderChart();
+
+const orange = "#DFB780";
+const bright_green = "#36D644";
+const muted_green = "#496753";
+
+const chartNode2: HTMLDivElement = document.querySelector("#chart2");
+const apexLineChartWarning = new ApexChartComponent(chartNode2, muted_green);
+apexLineChartWarning.renderChart();
